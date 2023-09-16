b7ce48bd6618efc887ca8c4a2b057675d80b4d21
Gurgulescu "Gusti" Augustin Mihai <118075768+GustiG@users.noreply.github.com>
2023-09-16 11:53:18 +0300

    Add files via upload
    from the book "Learning C++" by Haephrati Ruth

---
+++ 5_partial_template_specialization.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+template<typename T>
+struct SortData
+{
+    static void Sort(std::vector<T>& data)
+    {
+        std::sort(data.begin(), data.end());
+    }
+};
+
+template<typename T>
+struct SortData<T*>
+{
+    static void Sort(std::vector<T*>& data)
+    {
+        // #A
+        for (size_t i = 0; i < data.size() - 1; ++i)
+        {
+            for (size_t j = 0; j < data.size() - i - 1; ++j)
+            {
+                if (*data[j] > *data[j + 1])
+                {
+                    T* temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                }
+            }
+        }
+    }
+};
+
+int main()
+{
+    std::cout << "Welcome to Mr Sort. I sort things. Enter some numbers:" << std::endl;
+
+    std::vector<int> numbers;
+    int num;
+
+    // #B
+    std::string input;
+    std::getline(std::cin, input);
+    std::stringstream ss(input);
+    while (ss >> num)
+    {
+        numbers.push_back(num);
+    }
+    // #C
+    SortData<int>::Sort(numbers);
+    // #D
+    std::cout << "Sorted numbers: ";
+    for (const auto& num : numbers)
+    {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+    // #E
+    std::cout << "Sorting pointers: ";
+    std::vector<int*> pointers;
+    for (const auto& num : numbers)
+    {
+        pointers.push_back(new int(num));
+    }
+
+    SortData<int*>::Sort(pointers);
+    // #F
+    std::cout << "Sorted pointers: ";
+    for (const auto& ptr : pointers)
+    {
+        std::cout << *ptr << " ";
+        delete ptr;
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
+
+/*
+#A Sorting algorithm for pointers for example: Bubble Sort
+#B Read numbers from user input until a non-integer value is entered
+#C Sorting the numbers
+#D Printing the sorted numbers
+#E Sorting the pointers
+#F Printing the sorted pointers
+*/
