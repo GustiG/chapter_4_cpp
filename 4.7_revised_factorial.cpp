629409747bdabccf9c5b7fe2a5ea0b09e4422049
Gurgulescu "Gusti" Augustin Mihai <118075768+GustiG@users.noreply.github.com>
2023-09-26 08:44:30 +0300

    Add files via upload

---
+++ 4.7_revised_factorial.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // The factorial of a number
+    // 6! = 1*2*3*4*5*6 = 720
+
+    int number {};
+    cout << "Number: ";
+    cin >> number;
+    int factorial = 1;
+
+    for (int i = 1; i <= number; ++i ) 
+        factorial *= i;
+        
+    cout << number << " factorial = " << factorial << '\n';
+}
